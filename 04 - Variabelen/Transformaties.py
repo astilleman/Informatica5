c408e7e117bc008e43cdbca53cfbe5c992200f67
astilleman <43027764+astilleman@users.noreply.github.com>
2018-09-28 20:40:05 +0200

    Transformaties

---
+++ 04 - Variabelen/Transformaties.py
@@ -0,0 +1,10 @@
+#invoer
+a = int(input('Geef a: '))
+b = int(input('Geef b: '))
+
+#berekening
+functievoorschrift_g = 'g(x) = 2(x - ' + str(3 + a) + ')^2 + ' + str(4 + b)
+functievoorschrift_f = print('f(x) = 2(x - 3)^2 + 4')
+print(functievoorschrift_g)
+
+
