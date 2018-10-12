82ee884716b3791a5a46c48b2d2e231c8b5ca805
astilleman <43027764+astilleman@users.noreply.github.com>
2018-10-12 17:32:15 +0200

    Crocodile paradox

--- 06 - Condities/Crocodile paradox.py
+++ 06 - Condities/Crocodile paradox.py
@@ -2,5 +2,9 @@ stalen_zenuwen = input('Wie heeft stalen zenuwen?')
 
 if stalen_zenuwen == 'vader':
     print('Krokodil geeft kind terug')
+elif stalen_zenuwen == 'krokodil':
+    print('Krokodil eet kind op')
 else:
+    print('Moe van het denken')
+
 
