180cef4636adef26e79607f2f247a004e9026ce3
astilleman <43027764+astilleman@users.noreply.github.com>
2018-10-21 08:36:41 +0200

    Test

---
+++ 05 - EenvoudigeFuncties/Test.py
@@ -0,0 +1,42 @@
+#import math
+
+#getal = float(input('getal: '))
+
+#print(math.sqrt(getal) + math.sqrt(getal - 1)) TE LANG
+
+#####################################################
+
+#from math import sqrt as vierkantswortel
+
+#getal = float(input('getal: '))
+
+#print(vierkantswortel(getal) +  vierkantswortel(getal - 1))
+
+#vierkantswortel totaal overbodig as weg
+
+#######################################################
+
+#print(pi)
+
+##########################################
+
+
+#from random import random, randint, seed
+
+#print(random())
+#print(random())
+#print(random())
+#print(random())
+
+#willekeurig getal tussen 0 en 1 grenzen inbegrepen
+
+##############################################################
+
+#from random import random, randint, seed
+
+#seed(1267)
+
+#print(randint(10, 15))
+#print(randint(10, 15))
+#print(randint(10, 15))
+#print(randint(10, 15))
