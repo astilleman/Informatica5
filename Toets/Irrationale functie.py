ad6fd477d7379f54bd0a75c3da9a480970ec70ce
astilleman <43027764+astilleman@users.noreply.github.com>
2018-10-25 10:05:06 +0200

    Irrationale functie

---
+++ Toets/Irrationale functie.py
@@ -0,0 +1,13 @@
+from math import sqrt
+#invoer
+x = float(input('Geef een getal x: '))
+
+#bewerking en uitvoer
+if x < 2:
+    print('{:.2f} ∉ dom(f)'.format(x))
+
+elif x == 2:
+    print('2.00 is nulpunt van f')
+
+else:
+    print('f({:.2f}) = {:.2f}'.format(x, sqrt(x - 2)))
