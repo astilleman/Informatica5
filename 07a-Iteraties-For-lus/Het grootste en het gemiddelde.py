b5af3899ff467d5f1ccf54466e23f016fab4e309
astilleman <43027764+astilleman@users.noreply.github.com>
2018-11-09 23:03:36 +0100

    Het grootste en het gemiddelde

---
+++ 07a-Iteraties-For-lus/Het grootste en het gemiddelde.py
@@ -0,0 +1,30 @@
+#start
+n = int(input('Geef het aantal getallen: '))
+som, maximum  = 0, 0
+
+#berekeningen
+for i in range(n):
+    getallen = int(input('Getal: '))
+    som += getallen
+    if getallen > maximum :
+        maximum = getallen
+    elif maximum == 0:
+        maximum = getallen
+    else:
+        maximum = maximum
+
+
+gemiddelde = (1/n) * som
+
+#formattering
+mes = 'Het grootste getal is {} en het gemiddelde is {:.2f}'.format(maximum, gemiddelde)
+
+#uitvoer
+print(mes)
+
+
+
+
+
+
+
