b8b4bef6fa54d9b2a148d4b778659146bb5840dd
astilleman <43027764+astilleman@users.noreply.github.com>
2018-12-27 18:39:56 +0100

    BMI

--- Toets/BMI.py
+++ Toets/BMI.py
@@ -6,3 +6,24 @@ naam2 = str(input('Geef de naam van persoon 2: '))
 gewicht2 = float(input('Geef het gewicht van persoon 2: '))
 
 #berekening
+bmi = (max(gewicht1, gewicht2)) / (gelijke_lengte ** 2)
+
+if gewicht1 > gewicht2:
+    naam = naam1
+else:
+    naam = naam2
+
+if bmi < 18.5:
+    indicatie = 'heeft ondergewicht'
+elif 18.5 <= bmi < 25:
+    indicatie = 'heeft een gezond gewicht'
+elif 25 <= bmi <30:
+    indicatie = 'heeft overgewicht'
+else:
+    indicatie = 'is obees'
+
+#formattering
+mes = '{} heeft het hoogste BMI ({:.2f}) en {}.'.format(naam, bmi, indicatie)
+
+#uitvoer
+print(mes)
