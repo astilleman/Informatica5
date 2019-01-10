8010fc499397ee96315d276d9c701f8711637701
astilleman <43027764+astilleman@users.noreply.github.com>
2019-01-10 17:07:41 +0100

    Germaniseer

---
+++ 10 - Strings/Germaniseer.py
@@ -0,0 +1,18 @@
+def germaniseer(zin):
+
+    #alle letters overlopen
+    for i in range(0, len(zin) - 1):
+
+    # is het een spatie?
+        if zin[i] == ' ':
+        #ja -> volgende letter wordt een hoofdletter
+            zin = zin[0: i + 1] + zin[i + 1].upper() + zin[i + 2:]
+            #substring gaat nooit in fout, 1 letter wel
+
+    return zin
+
+
+
+
+
+print(germaniseer('Geluk helpt soms, moed altijd.'))
---
+++ 10 - Strings/Test.py
@@ -0,0 +1,10 @@
+woord = 'python'
+
+#for letter in woord:
+    #print(letter)
+
+#for i in range(0, len(woord), 2):
+    # je hoeft 0 er niet bij te typen maar duidelijk, voor begin
+    # telkens 1 minder dan de letters van het woord!
+    # letters overslaan -> letters met even index
+    #print(woord[i])
