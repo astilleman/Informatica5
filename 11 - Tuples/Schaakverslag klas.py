e88e5e1ebec52b1457ae7ab15d41658c2135df9c
astilleman <43027764+astilleman@users.noreply.github.com>
2019-02-08 23:05:00 +0100

    Schaakverslag klas

---
+++ 11 - Tuples/Schaakverslag klas.py
@@ -0,0 +1,20 @@
+def geldige_zet(zet):
+
+    mes = False
+
+    #controleren of index 0 in K, L, ...
+    if len(zet) == 3 and zet[0] in 'KDTLP' and zet[1] in 'abcdefgh' and zet[2] in '123456789':
+        mes = True
+    elif len(zet) == 2 and zet[0] in 'abcdefgh' and zet[1] in '123456789':
+        mes = True
+
+    return mes
+
+def geldige_zetten(zetten):
+
+    i = 0
+
+    while i < len(zetten) and geldige_zet(zetten[i]):
+        i += 1
+
+    return i >= len(zetten)
