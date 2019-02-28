64cdea6e0cfab51abd4354136ac24306de245471
astilleman <43027764+astilleman@users.noreply.github.com>
2019-02-28 09:20:11 +0100

    Synoniemen

---
+++ 13 - Dictionaries/Test 2.py
@@ -0,0 +1,20 @@
+
+#for k, v in fruitmand.items():
+    #print('ik heb ' + str(v) + ' ' + k)
+
+#.key() is enkel sleutels
+#.values() is enkel getal
+
+#klas = {1: 'arnout', 2: 'thibault', 'titularis': 'Mr. Van der Cruyssen'}
+
+#for k, v in klas.items():
+    #print(k,v)
+
+#heel diverse informatie
+#immutable, list kan dus nooit dictionary zijn , wel tuple, hieronder value wel
+
+#cirkel = {'middelpunt' : [0, 0], 'straal' : 5}
+#print(cirkel)
+
+#cirkel['middelpunt'][0] = 1
+#print(cirkel)
